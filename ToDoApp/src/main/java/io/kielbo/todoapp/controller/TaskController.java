2c29be3c9168e10b340e89e4d667a8c9066858cf
kielbo <pytkens@gmail.com>
2021-11-23 22:08:54 +0100

    up

--- ToDoApp/src/main/java/io/kielbo/todoapp/controller/TaskController.java
+++ ToDoApp/src/main/java/io/kielbo/todoapp/controller/TaskController.java
@@ -4,6 +4,9 @@ import io.kielbo.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 
 @RepositoryRestController
 public class TaskController {
@@ -12,5 +15,11 @@ public class TaskController {
 
     public TaskController(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+    @GetMapping(value = "/tasks", params = {"!sort","!page","!size"})
+    ResponseEntity<?> readAllTasks(){
+    	logger.warn("Exposing all the taska");
+    	return ResponseEntity.ok(taskRepository.findAll());
+
     }
 }
